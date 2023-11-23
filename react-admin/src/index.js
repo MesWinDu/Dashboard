b89f77e57e868f86151a6d4b186c742cd88c3e34
BestTanachat <tanachat.photilaksap@gmail.com>
2023-11-23 16:03:13 +0700

    line chart

--- react-admin/src/index.js
+++ react-admin/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom"
 
+// eslint-disable-next-line
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
-    //asdasdsadsad
     </BrowserRouter>
     
   </React.StrictMode>
