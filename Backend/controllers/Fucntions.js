73e0727c0f8befd71c42497967db650cd7453abd
MesWinDu <math7064@gmail.com>
2023-11-29 20:50:53 +0700

    Update

--- Backend/controllers/Fucntions.js
+++ Backend/controllers/Fucntions.js
@@ -9,7 +9,7 @@ function convertUnixTimestampToDateTime(unixTimestamp) {
     const day = ('0' + dateObject.getDate()).slice(-2);
     const hours = ('0' + dateObject.getHours()).slice(-2);
     const minutes = ('0' + dateObject.getMinutes()).slice(-2);
-    const seconds = ('0' + dateObject.getSeconds()).slice(-3);
+    const seconds = ('0' + dateObject.getSeconds()).slice(-2);
   
     // Construct the formatted date and time string
     const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
